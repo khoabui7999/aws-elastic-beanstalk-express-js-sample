580c43513615ebad9a07e7155f899db98de37b29
KhoaBui <khoabui.79.99@gmail.com>
2020-10-17 17:55:28 +0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS DEVOPS DEMO'));
+app.get('/', (req, res) => res.send('AWS DEVOPS EDITED'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
